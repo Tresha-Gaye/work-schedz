650985b73f37a9bfe3cc05e065c041f8c420b479
Twigster <treshagaye@gmail.com>
2021-12-27 20:35:54 -0500

    added input with focus for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
-var tasks = {};
+var tasks = "";
+var taskBox = $(".task-box");
+// task box was clicked
 
+
+// var createTask = function(taskText) {
+//    var taskP = $("<p>")
+//    .addClass("badge badge-primary badge-pill")
+//    .text(text);
+
+//    taskBox.append(taskP);
+
+
+// }
+
+// append task p element to task box parent div
+$(".lock-box").on("click", "div", function() {
+   console.log(click);
+});
+
+var taskInput = $("<textarea>")
+   .attr("type", "text")
+   .addClass("form-control bg-transparent text-white border-0 ml-auto  justify-content-center")
+   .val(tasks);
+
+   taskBox.append(taskInput);
+   taskInput.trigger("focus");
+
+
+   // $(".list-group").on("click", "p", function() {
+   //    var text = $(this)
+   //      .text()
+   //      .trim();
+   //    // console.log(text);
+   //    var textInput = $("<textarea>")
+   //      .addClass("form-control")
+   //      .val(text);
+      
 // **Function to display current and time at the top of the calendar**
 
 // var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -16,6 +52,7 @@ var datetime = null,
 var update = function() {
    date = moment(new Date())
    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+   dueTime();
 };
 
 $(document).ready(function() {
@@ -24,22 +61,69 @@ $(document).ready(function() {
    setInterval(update, 1000);
 });
 
+
+// create new input element for tasks
+
+//task box was clicked
+
+// $(".task-box").text("")
+// $("#due-9").click(function() {
+//    var taskInfo = $(".task-box")
+//    .text()
+//    .trim()
+// }
+
+   // var taskInput = (".task-box")
+   //    .attr("type", "text")
+   //    .addClass("task-input")
+   //    .val(tasks);
+
+// $(".task-box").on("click", "div", function() {
+//    // get current text
+//    var tasks = $(this)
+//      .text()
+//      .trim();
+ 
+// var taskInput = $(".task-box")
+//    .attr("type", "text")
+//    .addClass("task-box")
+//    .val(tasks);
+
+//   // swap out elements
+//   $(this).replaceWith(taskInput);
+
+// add color code to timeblocks for past, current or future events
+var dueTime = function () {
+   var getDueTime = new Date ($.now());
+   var currentHour = getDueTime.getHours();
+      // console.clear;
+   for (var i = 9; i < 18; i++) {
+      var idNum = i;
+      var taskTime = $("#due-" + idNum.toString());
+      if(currentHour > idNum) {
+         taskTime.css("background-color", "grey");
+      }
+      else if(currentHour == idNum) {
+         // console.log(currentHour, idNum);
+         taskTime.css("background-color", "red");     
+      }
+      else {
+         taskTime.css("background-color", "green");     
+      }
+   }
+};
+
 // var addTask = function() {
 
 // }
 
-var newTask = function () {
-   var newTaskLi = $("<li>").addClass("task-list-item");
-
-   var newTaskSpan = $("<span>")
-   .addClass("badge badge-primary badge-pill")
-   .text(taskDate);
-   console.log(taskDate);
-
-}
-
-
-var saveTasks = function() {
-   localStorage.setItem("tasks", JSON.stringify(tasks));
+var saveTasks = function(idNum) {
+   // localStorage.setItem("tasks", JSON.stringify(tasks));
+   var textValue = $("#text-" + idNum.toString()).val();
+   // console.log(textValue);
+   localStorage.setItem("text-" + idNum.toString(), textValue);
+   console.log(textValue);
 };
 
+// var loadTasks = function() {
+//    tasks = JSON.parse(localStorage.getItem("tasks"));
